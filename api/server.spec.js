55936b5a28d2670a7f77c7a49355c4caf8dd7444
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-18 13:52:12 -0300

    added login test

--- api/server.spec.js
+++ api/server.spec.js
@@ -18,4 +18,34 @@ describe("auth-router", () => {
         })
     })
   })
+
+  it("should added user to the db", () => {
+    return request(server)
+      .post("/api/auth/register")
+      .send({ username: "blue", password: "blue" })
+      .then((res) => {
+        expect(res.body.data.username).toBe("blue")
+      })
+  })
+
+  describe("login", () => {
+    it("user should be able to login", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .send({ username: "yellow", password: "yellow" })
+        .then((res) => {
+          expect(res.status).toBe(200)
+        })
+    })
+    it("user should be able to login", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .send({ username: "yellow", password: "yellow" })
+        .then((res) => {
+          expect(res.body.message).toBe("welcome back")
+        })
+    })
+  })
+    
+
 })
